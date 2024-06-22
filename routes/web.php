df4aa79303f2c7e5db121d6dd3a18ab730b17410
Laurinda <glaurinda00gnacadjalaurinda@gmail.com>
2024-06-22 15:24:05 +0300

    modif

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\DomainController;
 
+/*
 Route::get('/enter-domain', [DomainController::class, 'index']);
 Route::get('/domains/check', [DomainController::class, 'checkDomain'])->name('domains.check');
-Route::get('/domains', [DomainController::class, 'showDomains']);
-
 Route::get('/scrape-domain/{domainName}', [DomainController::class, 'runPythonScript']);
 Route::get('/scrape-results', [DomainController::class, 'showResults']);
-
 Route::post('/enter-domain', [DomainController::class, 'domainCheck']); // Traite le formulaire et vérifie le domaine
+*/
+
+Route::get('/domains', [DomainController::class, 'showDomains']);
 
 Route::get('/whois', [DomainController::class, 'getDomainInfo']);
 
